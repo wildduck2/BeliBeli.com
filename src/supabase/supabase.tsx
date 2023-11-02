cd128083940d61df8669480fb3c84cc91790a634
Ahmed Ayob abdo ali Ayob <108896341+thecoder512@users.noreply.github.com>
2023-11-02 20:37:31 +0200

    fixing the env

--- src/supabase/supabase.tsx
+++ src/supabase/supabase.tsx
@@ -1,11 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "../types/database.types";
 
-const PROJECT_URL = import.meta.env.NEXT_PUBLIC_SUPABASE_URL;
-const PROJECT_ANON_API_KEY = import.meta.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const PROJECT_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const PROJECT_ANON_API_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient<Database>(
   PROJECT_URL!,
   PROJECT_ANON_API_KEY!,
 );
-
