176a84c1f9788cd7493a9793db5e249ab0171ff4
Ahmed Ayob abdo ali Ayob <108896341+thecoder512@users.noreply.github.com>
2024-02-09 17:00:59 +0200

    style : egifts page

--- src/components/Layouts/EgifttsCards/EgifttsCards.tsx
+++ src/components/Layouts/EgifttsCards/EgifttsCards.tsx
@@ -2,27 +2,27 @@ import { Button } from "@/components/UI";
 import React from "react";
 
 const EgifttsCards = () => {
-  return (
-    <div className="account__Egiftts">
-      <h1>eGift Card</h1>
+    return (
+        <div className="account__Egifts">
+            <h1>eGift Card</h1>
 
-      <div className="account__Egiftts__form">
-        <h2>Link eGift card to account</h2>
-        <p>
-          You dont have any eGift card linked to your account, link card to use
-          it for your purchases
-        </p>
-        <span>
-          We'll send a verification code to your email to verify and link eGift
-          card
-        </span>
+            <div className="account__Egifts__form">
+                <h2>Link eGift card to account</h2>
+                <p>
+                    You dont have any eGift card linked to your account, link card to use
+                    it for your purchases
+                </p>
+                <span>
+                    We'll send a verification code to your email to verify and link eGift
+                    card
+                </span>
 
-        <input type="text" placeholder="eGift card number" />
+                <input type="number" min={0} placeholder="eGift card number" />
 
-        <Button variant={"default"}>Get Code</Button>
-      </div>
-    </div>
-  );
+                <Button variant={"default"}>Get Code</Button>
+            </div>
+        </div>
+    );
 };
 
 export default EgifttsCards;
